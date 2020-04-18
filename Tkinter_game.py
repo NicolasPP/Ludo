53733b7b51a4efd82a7aa0f4c7ee8b7397e4d7ba
Nicolas <nicolas.parucker@hotmail.com>
2020-04-18 16:05:05 +0200

    player cicles and lists

--- Tkinter_game.py
+++ Tkinter_game.py
@@ -76,16 +76,42 @@ for i in range(68):
      else:
           main_frame.create_text(main_frame.coords(tiles[i])[0]+10,main_frame.coords(tiles[i])[1]+10, text = i+1)
 
-print(tiles)
-test = main_frame.create_oval(370,370,400,400, fill = "white")
 
-move_object(main_frame,test,(600,600),25)
-print(main_frame.coords(test))
+
+
 top_left_rec = main_frame.create_rectangle(0,0,350,350, fill = "blue")
 top_right_rec = main_frame.create_rectangle(700,0,1050,350, fill = "Yellow")
 bottom_left_rec = main_frame.create_rectangle(0,700,350,1050, fill = "red")
 bottom_right_rec = main_frame.create_rectangle(700,700,1050,1050, fill = "green")
 
+
+## players
+blue = []
+
+blue.append(main_frame.create_oval(100,100,135,135,fill = "blue"))
+blue.append(main_frame.create_oval(250,250,215,215,fill = "blue"))
+blue.append(main_frame.create_oval(250,100,215,135,fill = "blue"))
+blue.append(main_frame.create_oval(100,250,135,215,fill = "blue"))
+
+yellow = []
+yellow.append(main_frame.create_oval(800,100,835,135,fill = "yellow"))
+yellow.append(main_frame.create_oval(950,250,915,215,fill = "yellow"))
+yellow.append(main_frame.create_oval(950,100,915,135,fill = "yellow"))
+yellow.append(main_frame.create_oval(800,250,835,215,fill = "yellow"))
+
+red = []
+
+red.append(main_frame.create_oval(100,800,135,835,fill = "red"))
+red.append(main_frame.create_oval(250,950,215,915,fill = "red"))
+red.append(main_frame.create_oval(250,800,215,835,fill = "red"))
+red.append(main_frame.create_oval(100,950,135,915,fill = "red"))
+
+green = []
+
+green.append(main_frame.create_oval(800,800,835,835,fill = "green"))
+green.append(main_frame.create_oval(950,950,915,915,fill = "green"))
+green.append(main_frame.create_oval(950,800,915,835,fill = "green"))
+green.append(main_frame.create_oval(800,950,835,915,fill = "green"))
 
 
 board.mainloop()
